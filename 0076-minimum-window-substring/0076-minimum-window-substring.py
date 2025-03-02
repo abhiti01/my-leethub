79a9093afb523f5d1e6971402ea78a5e6b8e38fe
abhiti01 <56045952+abhiti01@users.noreply.github.com>
2025-03-01 20:07:46 -0800

    Time: 75 ms (61.3%), Space: 18.2 MB (77.78%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,23 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        left =0
-        right=0
-        tFreq = collections.Counter(t)
-        sFreq={}
-        need = len(tFreq)
+        tCounter = collections.Counter(t)
+        need = len(tCounter)
         have=0
+        left=0
+        right=0
         lenAns = float('inf')
         ans=[-1,-1]
-        while(right<len(s)):
-            sFreq[s[right]] = sFreq.get(s[right],0)+1
-            if s[right] in tFreq and sFreq[s[right]] == tFreq[s[right]]:
+        sCounter = {}
+        while right<len(s):
+            sCounter[s[right]] = sCounter.get(s[right],0)+1
+            if s[right] in tCounter and sCounter[s[right]] == tCounter[s[right]]:
                 have+=1
             while have == need:
-                if right-left+1<lenAns:
+                if right - left +1 < lenAns:
                     lenAns = right-left+1
-                    ans=[left,right]
-                sFreq[s[left]]-=1
-                if s[left] in tFreq and sFreq[s[left]] < tFreq[s[left]]:
+                    ans[0],ans[1]= left,right
+                sCounter[s[left]] -=1
+                if s[left] in tCounter and sCounter[s[left]]<tCounter[s[left]]:
                     have-=1
                 left+=1
             right+=1
