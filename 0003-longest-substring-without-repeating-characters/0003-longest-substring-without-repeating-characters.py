cecccfd4affdbd98909153e8520b80503c0a1686
abhiti01 <56045952+abhiti01@users.noreply.github.com>
2025-03-01 23:03:47 -0800

    Time: 24 ms (26.13%), Space: 17.8 MB (53.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLength = 0
-        left=0
-        right=0
-        setChars=set()
-        while (right<len(s)):
-            while(s[right] in setChars):
-                setChars.remove(s[left])
+        if len(s)==0:
+            return 0
+        left = 0
+        right =0
+        windowLen=0
+        charCounter={}
+        maxLen=float('-inf')
+        while left<=right and right<len(s):
+            charCounter[s[right]] = charCounter.get(s[right],0)+1
+            while charCounter[s[right]]>1:
+                charCounter[s[left]] -=1
                 left+=1
-            setChars.add(s[right])
-            maxLength = max(maxLength,right-left+1)
+            windowLen = right-left+1
+            maxLen = max(maxLen,windowLen)
             right+=1
-            
-        return maxLength
-    #         other solution
-        # lastSeen = {}
-        # start = 0
-        # length = 0
-        # maxLength = 0
-        # i=0
-        # while i<len(s):
-        #     if s[i] in lastSeen and start<=lastSeen[s[i]]:
-        #         start = lastSeen[s[i]]+1
-        #         lastSeen[s[i]] = i
-        #     else:
-        #         lastSeen[s[i]] = i   
-        #     print(s[start:i+1])
-        #     length = i-start+1
-        #     maxLength = max(length,maxLength)
-        #     i+=1
-        # return maxLength
+        return maxLen
