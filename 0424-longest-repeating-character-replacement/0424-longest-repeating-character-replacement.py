b309a33a826b5ea64f25dba1c36ddd73193c1651
abhiti01 <56045952+abhiti01@users.noreply.github.com>
2025-03-01 23:10:06 -0800

    Time: 140 ms (47.53%), Space: 18.1 MB (48.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,15 +3,17 @@ class Solution:
         l=0
         r=0
         dic={}
-        ans = 0
-        while (r<len(s)):
-            dic[s[r]] = dic.get(s[r],0)+1
+        ans=0
+        while(r<len(s)):
+            c = s[r]
+            dic[c] = dic.get(c,0)+1
             cells = r-l+1
             cost = cells-max(dic.values())
-            if(cost>k):
+            if cost>k:
                 dic[s[l]]-=1
                 l+=1
             else:
-                ans = max(ans,r-l+1)
+                ans=max(ans,r-l+1)
             r+=1
         return ans
+                
