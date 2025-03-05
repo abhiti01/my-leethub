1a544bf8861e46261e6f2326594281d2bb637a37
abhiti01 <56045952+abhiti01@users.noreply.github.com>
2025-03-04 21:45:20 -0800

    Time: 3 ms (6.94%), Space: 18.5 MB (95.83%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,11 +7,34 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
-        while curr is not None:
-            new = curr.next
+        
+        while curr:
+            next_node = curr.next
             curr.next = prev
             prev = curr
-            curr = new
-            # head = curr
+            curr = next_node
             
         return prev
+
+        # another way of reversing a list - traverse and insert each node at the head
+
+
+        # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #     curr = head
+        #     prev = None
+        #     while curr:
+
+        #         # Keep track of the next node to process in the
+        #         # original list
+        #         next_node = curr.next
+
+        #         # Insert the node pointed to by "curr"
+        #         # at the beginning of the reversed list
+        #         curr.next = prev
+        #         prev = curr
+
+        #         # Move on to the next node
+        #         curr = next_node
+
+        #     # Return the head of the reversed list
+        #     return prev
