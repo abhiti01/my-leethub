1e945a46b88f642e306f13fc07e8be6b745c2e16
abhiti01 <56045952+abhiti01@users.noreply.github.com>
2025-03-04 21:47:16 -0800

    Time: 3 ms (32.4%), Space: 18.6 MB (40.82%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -4,28 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # def reverse (self, a, b):
-    #     pre = None
-    #     cur = a
-    #     nxt = a
-    #     while cur != b:
-    #         nxt = cur.next
-    #         cur.next = pre
-    #         pre = cur
-    #         cur = nxt
-    #     return pre
+    def reverse (self, a, b):
+        pre = None
+        cur = a
+        nxt = a
+        while cur != b:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
 
-    # def reverseKGroup(self, head, k):
-    #     if head == None:
-    #         return None
-    #     a = b = head
-    #     for i in range(k):
-    #         if b == None:
-    #             return head
-    #         b = b.next
-    #     newHead = self.reverse(a,b)
-    #     a.next = self.reverseKGroup(b,k)
-    #     return newHead
+    def reverseKGroup(self, head, k):
+        if head == None:
+            return None
+        a = b = head
+        for i in range(k):
+            if b == None:
+                return head
+            b = b.next
+        newHead = self.reverse(a,b)
+        a.next = self.reverseKGroup(b,k)
+        return newHead
 
         #O(1) space solution (non-recursive)
 
@@ -36,42 +36,42 @@ class Solution:
     #             prev
     #                   curr
     # """
-        def reverseList(self, head, k):
-            prev = None
-            curr = head
+        # def reverseList(self, head, k):
+        #     prev = None
+        #     curr = head
             
-            for _ in range(k):
-                next_node = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_node
+        #     for _ in range(k):
+        #         next_node = curr.next
+        #         curr.next = prev
+        #         prev = curr
+        #         curr = next_node
             
-            return prev, curr
+        #     return prev, curr
 
-        def reverseKGroup(self, head, k):
-            dummy = ListNode(0)
-            dummy.next = head
-            ptr = dummy
+        # def reverseKGroup(self, head, k):
+        #     dummy = ListNode(0)
+        #     dummy.next = head
+        #     ptr = dummy
             
-            while ptr is not None:
-                tracker = ptr
+        #     while ptr is not None:
+        #         tracker = ptr
                 
-                # Check if there are k nodes to reverse
-                for _ in range(k):
-                    tracker = tracker.next
-                    if tracker is None:
-                        return dummy.next # If fewer than k nodes, return result
+        #         # Check if there are k nodes to reverse
+        #         for _ in range(k):
+        #             tracker = tracker.next
+        #             if tracker is None:
+        #                 return dummy.next # If fewer than k nodes, return result
                 
-                # Reverse k nodes
-                prev, curr = self.reverseList(ptr.next, k)
+        #         # Reverse k nodes
+        #         prev, curr = self.reverseList(ptr.next, k)
                 
-                last_node_of_reversed_group = ptr.next # to link to next group
-                last_node_of_reversed_group.next = curr # link to next group
-                ptr.next = prev # point ptr to prev, the new head
+        #         last_node_of_reversed_group = ptr.next # to link to next group
+        #         last_node_of_reversed_group.next = curr # link to next group
+        #         ptr.next = prev # point ptr to prev, the new head
                 
-                ptr = last_node_of_reversed_group # Move ptr forward
+        #         ptr = last_node_of_reversed_group # Move ptr forward
             
-            return dummy.next
+        #     return dummy.next
 
 
         
